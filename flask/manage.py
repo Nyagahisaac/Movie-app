a1d923df56d8f07ae4d76e06b6071d22fc1a9f0a
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-09-10 00:46:06 +0300

    flask

--- flask/manage.py
+++ flask/manage.py
@@ -14,23 +14,7 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     
     
     
